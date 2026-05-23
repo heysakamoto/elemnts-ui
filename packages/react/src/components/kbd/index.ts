f4be38e5b9392b41e623d15f285fe7b054706fd7
heysakamoto <heyolanrewaju@gmail.com>
2026-05-23 22:19:15 +0100

    refactor(react): make kbd semantic and add groups

--- packages/react/src/components/kbd/index.ts
+++ packages/react/src/components/kbd/index.ts
@@ -1,18 +1,24 @@
 import type { ComponentProps } from "react";
-import { KbdItem, KbdRoot } from "./kbd";
+import { KbdItem, KbdItemGroup, KbdItemGroupText, KbdRoot } from "./kbd";
 
 export type KbdRootProps = ComponentProps<typeof KbdRoot>;
 export type KbdItemProps = ComponentProps<typeof KbdItem>;
+export type KbdItemGroupProps = ComponentProps<typeof KbdItemGroup>;
+export type KbdItemGroupTextProps = ComponentProps<typeof KbdItemGroupText>;
 
 export const Kbd = Object.assign(KbdRoot, {
 	Item: KbdItem,
 	Root: KbdRoot,
+	ItemGroup: KbdItemGroup,
+	ItemGroupText: KbdItemGroupText,
 });
 
 export type Kbd = {
 	RootProps: KbdRootProps;
 	ItemProps: KbdItemProps;
+	ItemGroupProps: KbdItemGroupProps;
+	ItemGroupTextProps: KbdItemGroupTextProps;
 	Props: KbdRootProps;
 };
 
-export { KbdRoot, KbdItem };
+export { KbdRoot, KbdItem, KbdItemGroup, KbdItemGroupText };
