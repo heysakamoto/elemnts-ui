54a0eabe41b0d0e90642d08dfa59e4385421f96a
heysakamoto <heyolanrewaju@gmail.com>
2026-05-24 21:45:01 +0100

    docs(docs): add data-list documentation and demos

---
+++ apps/docs/src/demos/data-list/horizontal.tsx
@@ -0,0 +1,22 @@
+import { DataList } from "@moto-ui/react";
+
+export function Horizontal() {
+	return (
+		<DataList.Root orientation="horizontal">
+			<DataList.Item>
+				<DataList.ItemLabel>Name</DataList.ItemLabel>
+				<DataList.ItemValue>Moto UI</DataList.ItemValue>
+			</DataList.Item>
+			<DataList.Item>
+				<DataList.ItemLabel>Description</DataList.ItemLabel>
+				<DataList.ItemValue>
+					A React component library built with Ark UI and Panda CSS.
+				</DataList.ItemValue>
+			</DataList.Item>
+			<DataList.Item>
+				<DataList.ItemLabel>License</DataList.ItemLabel>
+				<DataList.ItemValue>MIT</DataList.ItemValue>
+			</DataList.Item>
+		</DataList.Root>
+	);
+}
