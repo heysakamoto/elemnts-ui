74ce63a908af53b6b5b6abb59a36f115f48324a3
heysakamoto <heyolanrewaju@gmail.com>
2026-05-25 14:40:44 +0100

    fix(docs): simplify basic demo

--- apps/docs/src/demos/virtual-list/basic.tsx
+++ apps/docs/src/demos/virtual-list/basic.tsx
@@ -1,23 +1,20 @@
 import { Container, DecorativeBox, VirtualList } from "@moto-ui/react";
-import { ClientOnly } from "@tanstack/react-router";
 
 export function Basic() {
 	return (
 		<Container maxW="18rem">
-			<ClientOnly>
-				<VirtualList.Root
-					count={1000}
-					estimateSize={() => 35}
-				>
-					<VirtualList.Viewport style={{ height: 320 }}>
-						<VirtualList.Container>
-							<VirtualList.Items>
-								{(index) => <DecorativeBox h="full">{index}</DecorativeBox>}
-							</VirtualList.Items>
-						</VirtualList.Container>
-					</VirtualList.Viewport>
-				</VirtualList.Root>
-			</ClientOnly>
+			<VirtualList
+				count={1000}
+				estimateSize={() => 35}
+			>
+				<VirtualList.Viewport style={{ height: 320 }}>
+					<VirtualList.Container>
+						<VirtualList.Items>
+							{(index) => <DecorativeBox h="full">{index}</DecorativeBox>}
+						</VirtualList.Items>
+					</VirtualList.Container>
+				</VirtualList.Viewport>
+			</VirtualList>
 		</Container>
 	);
 }
