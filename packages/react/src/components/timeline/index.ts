81396581b9aad557dec5c91cc118153b496cb635
heysakamoto <heyolanrewaju@gmail.com>
2026-05-27 10:57:56 +0100

    refactor(react): simplify sortable and timeline exports

--- packages/react/src/components/timeline/index.ts
+++ packages/react/src/components/timeline/index.ts
@@ -2,34 +2,26 @@ import type { ComponentProps } from "react";
 import {
 	TimelineConnector,
 	TimelineContent,
-	TimelineDescription,
 	TimelineIndicator,
 	TimelineItem,
 	TimelineRoot,
 	TimelineSeparator,
-	TimelineTitle,
 } from "./timeline";
 
 export type TimelineConnectorProps = ComponentProps<typeof TimelineConnector>;
 export type TimelineContentProps = ComponentProps<typeof TimelineContent>;
-export type TimelineDescriptionProps = ComponentProps<
-	typeof TimelineDescription
->;
 export type TimelineIndicatorProps = ComponentProps<typeof TimelineIndicator>;
 export type TimelineItemProps = ComponentProps<typeof TimelineItem>;
 export type TimelineRootProps = ComponentProps<typeof TimelineRoot>;
 export type TimelineSeparatorProps = ComponentProps<typeof TimelineSeparator>;
-export type TimelineTitleProps = ComponentProps<typeof TimelineTitle>;
 
 export const Timeline = Object.assign(TimelineRoot, {
 	Root: TimelineRoot,
 	Connector: TimelineConnector,
 	Content: TimelineContent,
-	Description: TimelineDescription,
 	Indicator: TimelineIndicator,
 	Item: TimelineItem,
 	Separator: TimelineSeparator,
-	Title: TimelineTitle,
 });
 
 export type Timeline = {
@@ -37,20 +29,16 @@ export type Timeline = {
 	RootProps: TimelineRootProps;
 	ConnectorProps: TimelineConnectorProps;
 	ContentProps: TimelineContentProps;
-	DescriptionProps: TimelineDescriptionProps;
 	IndicatorProps: TimelineIndicatorProps;
 	ItemProps: TimelineItemProps;
 	SeparatorProps: TimelineSeparatorProps;
-	TitleProps: TimelineTitleProps;
 };
 
 export {
 	TimelineRoot,
 	TimelineConnector,
 	TimelineContent,
-	TimelineDescription,
 	TimelineIndicator,
 	TimelineItem,
 	TimelineSeparator,
-	TimelineTitle,
 };
