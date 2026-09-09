92d71f3e54643b23378f06bb5aa2d0f12d9d0922
heysakamoto <heyolanrewaju@gmail.com>
2026-09-09 13:50:06 +0100

    refactor(presets): update folder structure

---
+++ packages/core/src/presets/base.ts
@@ -0,0 +1,12 @@
+import { definePreset } from "@pandacss/dev";
+
+import { config, theme } from "../registry";
+
+const basePreset = definePreset({
+  theme,
+  ...config,
+  name: "@elemnts-ui/preset-base",
+  presets: ["@pandacss/preset-base"],
+});
+
+export default basePreset;
